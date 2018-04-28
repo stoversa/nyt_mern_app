c905ecac7b2d7feb02b679aad04a19850c448ed2
stoversa <stoversa@gmail.com>
2018-04-28 09:29:35 -0400

    fix didn't work

--- server.js
+++ server.js
@@ -21,7 +21,11 @@ mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 // Send every request to the React app
-// Define any API routes before this runs
+// Define any API routes before this runs..
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
