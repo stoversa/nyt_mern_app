8bbd9862efb6e82522d065d5dd12d0741f80ea6b
stoversa <stoversa@gmail.com>
2018-04-28 09:00:53 -0400

    Deployment mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 import Main from "./components/Main"
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <Main />
+      <Router>
+        <div>
+          <Route exact path='/' component={Main} />
+        </div>
+      </Router>
     );
   }
 }
