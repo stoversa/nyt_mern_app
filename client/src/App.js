aea2cb24a52b01abfc2ab9d1f3f22ee40c14de61
stoversa <stoversa@gmail.com>
2018-04-29 20:46:44 -0400

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./pages/Main";
 import Saved from "./pages/Saved";
-import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
@@ -13,7 +12,6 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Main} />
         <Route exact path="/saved" component={Saved} />
-        <Route exact path="/saved/:id" component={Detail} />
         <Route component={NoMatch} />
       </Switch>
     </div>
